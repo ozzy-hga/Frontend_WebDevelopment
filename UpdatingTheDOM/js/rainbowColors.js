3cb45423a509b00e89e30cc5b0138c4dfa7cb71a
Osvaldo Museta <88104483+ozzy-hga@users.noreply.github.com>
2021-09-15 21:24:29 +0200

    Interactive Rainbow Challenge

---
+++ UpdatingTheDOM/js/rainbowColors.js
@@ -0,0 +1,37 @@
+function changeColorOne() {
+    document.getElementById("colorOne").style.backgroundColor = "red";
+    document.getElementById("emptyText1").innerHTML = "<h1>Red</h1>";
+}
+
+function changeColorTwo() {
+    document.getElementById("colorTwo").style.backgroundColor = "orange";
+    document.getElementById("emptyText2").innerHTML = "<h1>Orange</h1>";
+}
+
+function changeColorThree() {
+    document.getElementById("colorThree").style.backgroundColor = "yellow";
+    document.getElementById("emptyText3").innerHTML = "<h1>Yellow</h1>";
+}
+
+function changeColorFour() {
+    document.getElementById("colorFour").style.backgroundColor = "green";
+    document.getElementById("emptyText4").innerHTML = "<h1>Green</h1>";
+}
+
+function changeColorFive() {
+    document.getElementById("colorFive").style.backgroundColor = "blue";
+    document.getElementById("emptyText5").innerHTML = "<h1>Blue</h1>";
+}
+
+function changeColorSix() {
+    document.getElementById("colorSix").style.backgroundColor = "indigo";
+    document.getElementById("emptyText6").innerHTML = "<h1>Indigo</h1>";
+}
+
+function changeColorSeven() {
+    document.getElementById("colorSeven").style.backgroundColor = "violet";
+    document.getElementById("emptyText7").innerHTML = "<h1>Violet</h1>";
+}
+
+
+
